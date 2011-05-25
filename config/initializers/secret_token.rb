fe0ec69ba73808ff3d2ee62786a979560f78c2a7
Michael Hearn <michael.e.hearn@gmail.com>
2011-05-25 15:06:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySwoops::Application.config.secret_token = '8aeec7bd06b37ebff92f24132d49399bb65fab2d161699bd76995149833f74a664930200ea268712944b69b11d1b07774ffe69572f26bc66125868b5fb627599'

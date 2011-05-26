769c34e863f9d72c485ec07fe6d8d3777cecddc9
Michael Hearn <michael.e.hearn@gmail.com>
2011-05-25 21:57:31 -0400

    Initial commit on Mac

---
+++ README.markdown
@@ -0,0 +1,5 @@
+== Welcome to mySwoops ==
+
+mySwoops is a place where avid motorcyclists can track their swoops. Swoops are defined as leisurely motorcycle rides.
+
+You can find more info at [www.myswoops.com](http://www.myswoops.com) or you can email [me](mailto://michael.e.hearn@gmail.com) for more info.
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MySwoops::Application.config.session_store :cookie_store, :key => '_mySwoops_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MySwoops::Application.config.session_store :active_record_store
